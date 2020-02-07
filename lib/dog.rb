68d9947f0d5778b1de8e0048f2820bdf2e78f187
Rick Belson <belsonrj@aol.com>
2020-02-07 01:32:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,21 @@
 class Dog
-  def initialize(breed)
+  def initialize(breed, name)
     @breed = breed
   end
  
   def breed=(breed)
     @breed = breed
   end
+  
+  def name=(name)
+    @name = name
+  end
  
   def breed
     @breed
   end
+  
+  def name 
+    @name
+  end 
 end
