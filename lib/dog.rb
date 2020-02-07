ff86dd91dd6057e21b47a2f2b0ad87522d780f9f
Rick Belson <belsonrj@aol.com>
2020-02-07 01:38:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,22 +2,15 @@ class Dog
   def initialize(breed)
     @breed = breed
   end
-
+ 
   def breed=(breed)
     @breed = breed
-  end
-  
-  def name=(name)
-    @name = name
   end
  
   def breed
     @breed
   end
-  
-  def name 
-    @name
-  end 
 end
 
 Fido = Dog.new 
+
